6a8334229ea0f1edf6d4d3ff0fc7db2bec9343a6
Ashvin Perera <ashvin.perera.97@gmail.com>
2025-06-24 13:52:20 +0100

    Create package_manager.R

---
+++ code/package_manager.R
@@ -0,0 +1,22 @@
+# ------------------------------------------------------------------------------
+# Import All Used Packages 
+# ------------------------------------------------------------------------------
+
+library(tidyverse)
+library(haven)
+library(patchwork)
+library(hrbrthemes)
+library(ggrepel)
+library(lfe)
+library(stargazer)
+library(broom)
+library(here)
+library(ggplot2)
+library(fixest)
+library(scales)
+library(sf)
+library(spdep)
+library(bacondecomp)
+library(did)
+library(kableExtra)
+library(car)
